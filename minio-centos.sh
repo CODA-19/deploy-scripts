927190da363893e334fc6ade058879b29df49971
Louis Mullie <louis.mullie@gmail.com>
2020-07-25 12:49:04 -0400

    Create minio-centos.sh

---
+++ minio-centos.sh
@@ -0,0 +1,15 @@
+# Test standalone MinIO on Docker (not persistent)
+docker run -p 9000:9000 \
+  -e "MINIO_ACCESS_KEY=AKIAIOSFODNN7EXAMPLE" \
+  -e "MINIO_SECRET_KEY=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY" \
+  minio/minio server /data
+  
+# Test dashboard: localhost:9000
+
+# Run MinIO on Docker with persistent storage on a mount
+docker run -p 9000:9000 \
+  --name minio1 \
+  -v /mnt/data:/data \
+  -e "MINIO_ACCESS_KEY=AKIAIOSFODNN7EXAMPLE" \
+  -e "MINIO_SECRET_KEY=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY" \
+  minio/minio server /data
