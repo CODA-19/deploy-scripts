98fe4cefff6f1bcd4af7deb583f87c24dfb8ea96
Bruno Lavoie <bruno.lavoie@dti.ulaval.ca>
2020-09-03 09:51:09 -0400

    Import ul_aide and ul_stig ansible roles, add Vagrant file for local development

---
+++ ansible/roles/ul_aide/files/init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+FILEFLAG=/var/lib/aide/.init
+
+if [ ! -f $FILEFLAG ]; then
+    nice -n 19 aide --init && \
+    mv -f /var/lib/aide/aide.db.new.gz /var/lib/aide/aide.db.gz && \
+    touch $FILEFLAG
+fi
