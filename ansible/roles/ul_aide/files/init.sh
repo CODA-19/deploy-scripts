ff2e6113f850cf0779e03a501a3554c529894f8f
Bruno Lavoie <bruno.lavoie@dti.ulaval.ca>
2020-10-22 11:02:53 -0400

    ul_aide: add executable full path

--- ansible/roles/ul_aide/files/init.sh
+++ ansible/roles/ul_aide/files/init.sh
@@ -3,7 +3,7 @@
 FILEFLAG=/var/lib/aide/.init
 
 if [ ! -f $FILEFLAG ]; then
-    nice -n 19 aide --init && \
+    nice -n 19 /usr/sbin/aide --init && \
     mv -f /var/lib/aide/aide.db.new.gz /var/lib/aide/aide.db.gz && \
     touch $FILEFLAG
 fi
